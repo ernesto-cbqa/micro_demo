e983611ecab88bd1c55b368af7c5f19a56395ad6
ernesto-cbqa <ernesto-cbqa@users.noreply.github.com>
2015-06-02 12:26:00 -0500

    Delete hello_world_spec.rb

--- GenericRubyFramework/spec/hello_world_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'spec_helper'
-
-RSpec.describe "Parallel testing", :tr_id => '1', :parallel => true do
-  
-  it "should print hello world" do
-    puts "hello world"
-  end
-  
-end
